1c3cf515bf3e516ebc3614bb7872abb6809a1a4a
kathirzexz <kathirzexz@gmail.com>
2025-12-11 01:36:22 -0800

    Create MyLibrary.h

---
+++ MyLibrary.h
@@ -0,0 +1,7 @@
+💡 Summary:
+
+1.Servo.h → Already in IDE , Search Your Arduino Ide - Include Library - Search Servo.h - Download And Add
+
+
+2.AFMotor.h → Must install manually
+You Can Download On Your Browser!!
